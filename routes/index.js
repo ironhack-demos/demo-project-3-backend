4bba86ee2ee0fdc4410ff047c6d20ace5b066ba0
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2024-09-20 10:32:54 +0200

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+
+router.get("/", (req, res, next) => {
+  res.json("All good in here");
+});
+
+router.get('/health', (req, res) => {
+  res.status(200).json({
+      status: "OK",
+      uptime: process.uptime(),
+      timestamp: new Date().toISOString()
+  });
+});
+
+module.exports = router;
