c15cd4948c436eb70bd1d1743a3ffcd04958c9fa
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2024-09-20 11:24:16 +0200

    chore: Remove health endpoint from index.js

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,4 @@ router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
-router.get('/health', (req, res) => {
-  res.status(200).json({
-      status: "OK",
-      uptime: process.uptime(),
-      timestamp: new Date().toISOString()
-  });
-});
-
 module.exports = router;
